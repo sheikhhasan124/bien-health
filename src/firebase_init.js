46ac0b031eb50ae0f5ac5bbb979b63a3b469edcf
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-17 06:25:23 +0600

    initial

---
+++ src/firebase_init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiOmUDBmnA7pmLnMs-hC-svrKOE_G1EBo",
+  authDomain: "bien-health-4c6ec.firebaseapp.com",
+  projectId: "bien-health-4c6ec",
+  storageBucket: "bien-health-4c6ec.appspot.com",
+  messagingSenderId: "34996737271",
+  appId: "1:34996737271:web:43b065c316a2508ff7eff2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
